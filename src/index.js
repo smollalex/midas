ec213370fe8f90a1fe9f3fcc003615f019dfd6be
smollalex <smollalex86@gmail.com>
2020-12-11 10:24:37 +0600

    index page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import 'bootstrap'
-import './sass/main.scss'
+import "bootstrap";
 
+import "owl.carousel/src/scss/owl.carousel.scss";
+import "owl.carousel";
+
+import "./sass/main.scss";
+
+$(document).ready(function () {
+  $(".owl-theme-main").owlCarousel({
+    loop: true,
+    nav: true,
+    mouseDrag: true,
+    dots: true,
+    items: 1,
+    lazyLoad: true,
+   
+  });
+  $(".owl-theme-trends").owlCarousel({
+    loop: true,
+    nav: true,
+    mouseDrag: true,
+    dots: true,
+    items: 4,
+    lazyLoad: true,
+    margin: 40,
+   
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,19 @@ module.exports = (env, options) => ({
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
-              outputPath: "img/"
+              outputPath: "images"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
             }
           }
         ]
